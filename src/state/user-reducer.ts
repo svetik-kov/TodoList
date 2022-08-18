e3eda2c4f6b127ca8fde8b8bd2cc1216306bcaad
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-08-18 11:35:24 +0300

    test1 for reducer(иммутабельно)

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -14,8 +14,9 @@ type ActionType = {
 export const userReducer = (state: StateType, action: ActionType) => {
     switch (action.type) {
         case 'INCREMENT-AGE':
-            state.age = state.age + 1
-            return state
+            /*state.age = state.age + 1*/
+            return {...state,age:state.age + 1}
+
         case 'INCREMENT-CHILDREN-COUNT':
             state.childrenCount = state.childrenCount + 1
             return state
