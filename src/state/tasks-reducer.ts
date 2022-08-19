7e9f2f5a4db6be1c2ec962357a36c421958a2999
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-08-19 14:16:15 +0300

    test-reducer for addTaskAC

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,10 +1,13 @@
 import {TasksStateType} from "../App";
+import {v1} from "uuid";
 
 
 type TasksReducerType=
     removeTasksACType
+    |addTaskACType
       ;
 type removeTasksACType=ReturnType<typeof removeTaskAC>
+type addTaskACType=ReturnType<typeof addTaskAC>
 
 
 
@@ -14,6 +17,12 @@ export const tasksReducer=(state:TasksStateType,action:TasksReducerType):TasksSt
            return {...state,
                [action.payload.todolistId]
                    :state[action.payload.todolistId].filter(el=>el.id!==action.payload.id)}
+       }
+       case "ADD-TASK":{
+           let newTask = {id: v1(), title: action.payload.title, isDone: false};
+           return {...state,
+               [action.payload.todolistId]:
+           [newTask,...state[action.payload.todolistId]]}
        }
        default: return state
    }
@@ -25,4 +34,10 @@ export const removeTaskAC=(id: string, todolistId: string)=>{
         payload: {id, todolistId}
     }as const
 }
+export const addTaskAC=(title: string, todolistId: string)=>{
+    return{
+        type:'ADD-TASK',
+        payload:{title, todolistId}
 
+    }as const
+}
