ad8f43c43db2bbf21b4ebef70bbb806a1042f0e4
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-08-19 12:46:28 +0300

    test-reducer for addTodolistAC

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,15 +1,20 @@
 import {TodolistType} from "../App";
+import {v1} from "uuid";
 
-type TodolistReducerType=removeTodolistACType
+type TodolistReducerType=removeTodolistACType|addTodolistACType
 type removeTodolistACType=ReturnType<typeof removeTodolistAC>
-
+type addTodolistACType=ReturnType<typeof addTodolistAC>
 
 export const todolistsReducer=(state:Array<TodolistType>,action:TodolistReducerType)=>{
    switch (action.type) {
        case 'REMOVE-TODOLIST':{
            return state.filter(el=>el.id!==action.id)
        }
-           default: return state
+       case "ADD-TODOLIST":{
+           let newTodolist: TodolistType = {id: v1(), title:action.title, filter: 'all'};
+           return [...state,newTodolist]
+       }
+       default: return state
    }
 }
 
@@ -17,5 +22,11 @@ export const removeTodolistAC=(todolistId1: string)=>{
     return{
         type:'REMOVE-TODOLIST',
         id: todolistId1
+    }as const
+}
+export const addTodolistAC=(newTodolistTitle:string)=>{
+    return {
+        type: 'ADD-TODOLIST',
+        title: newTodolistTitle
     }as const
 }
